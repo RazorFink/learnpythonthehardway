8faa3b710d9863fb6243c3f2ede70d33e3d2a6db
Matthew Howe <mhowe@bates.edu>
2016-02-27 09:46:31 -0500

    add comments

--- ex7.py
+++ ex7.py
@@ -1,8 +1,13 @@
+# print simple string
 print "Mary had a little lamb."
+# print formatted string
 print "Its fleece was white as %s." % 'snow'
+# print simple string
 print "And everywhere that Mary went."
+# print 10 periods
 print "." * 10  # what'd that do?
 
+# assign a single character to a variable
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -17,5 +22,7 @@ end11 = "e"
 end12 = "r"
 
 # watch that comma at the end.  try removing it to see what happens
+# print concatenation of strings
 print end1 + end2 + end3 + end4 + end5 + end6,
+# print concatenation of strings, comma keeps same line
 print end7 + end8 + end9 + end10 + end11 + end12
